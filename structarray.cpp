86281ee7311885636500a41f623600c70bf550a8
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-07 15:53:40 +0700

    struct

--- structarray.cpp
+++ structarray.cpp
@@ -2,3 +2,21 @@
 #include <string>
 using namespace std;
 
+struct alamatDetail
+{
+    string desa;
+    string kota;
+
+};
+
+struct mahasiswa 
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main ()
+{
+  
+}
