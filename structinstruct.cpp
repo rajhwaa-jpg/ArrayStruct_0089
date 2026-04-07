fa580024b9bf7bff674fcdc81f99a6963a7e814e
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-07 15:58:32 +0700

    isi obyek

--- structinstruct.cpp
+++ structinstruct.cpp
@@ -16,5 +16,15 @@ int main() {
     // Deklarasi obyek dari struct orang
     orang mhs;
 
+    // Isi objek
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
    
 }
