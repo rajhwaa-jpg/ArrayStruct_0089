99898041a0fd3c363ca6eeb87e1c4e4085b0ba38
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-07 15:57:49 +0700

    struct

--- structinstruct.cpp
+++ structinstruct.cpp
@@ -1,3 +1,17 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat {
+    string desa;
+    string kota;
+};
+
+struct orang {
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main() {
+ 
+}
