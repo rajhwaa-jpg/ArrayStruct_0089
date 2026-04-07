789929c323024c41ac38a4d9b10237f401092f2f
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-07 15:44:43 +0700

    Add struct examples for mahasiswa and orang with address details
    - Created structarray.cpp to demonstrate an array of mahasiswa structs, including nested alamatDetail struct for address information.
    - Implemented user input for multiple mahasiswa entries and displayed their details.
    - Created structinstruct.cpp to illustrate a single orang struct with address details, including user input and output of the object's properties.

--- array.cpp
+++ array.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int main(){
+     //Deklarasi Array dengan inisialisasi
+     int dataku[5] = {10, 20, 30, 40, 50};
+     //deklarasi tanpa inisialisasi
+     int datamu[5];
+     int x;
+     //cara membaca array dataku
+     cout << "Data ke - 3 : " << dataku[2]<<endl;
+
+     //Operasi Matematika
+     x = dataku[2]*3;
+     //cara menulis atau mengisi array
+     dataku[2] = 600;
+     cout << "Data ke - 3 yang baru : " << dataku[2]<<endl;
+
+     
+}
