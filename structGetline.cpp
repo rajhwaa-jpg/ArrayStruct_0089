b0f7359ed7d9bad0c24218adc23bda940128fefd
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-04-07 15:55:43 +0700

    struct

--- structGetline.cpp
+++ structGetline.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+struct DetailAlamat{
+    string desa;
+    string kota;
+};
+struct orang{
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+int main()
+{
+    
+}
